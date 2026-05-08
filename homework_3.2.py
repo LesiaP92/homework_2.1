4b25b46881a71aef38d3981039b29df5e919b82c
LesiaP92 <lesja_p92@icloud.com>
2026-05-08 11:44:09 +0300

    homework_3.2

--- homework_3.2.py
+++ homework_3.2.py
@@ -1,4 +1,5 @@
 list_numbers = [1, 2, 3, 4, 5]
-x = list_numbers.pop()
-list_numbers.insert(0, 5)
+if len (list_numbers) > 1:
+    last_element = list_numbers.pop()
+    list_numbers.insert(0, last_element)
 print(list_numbers)
